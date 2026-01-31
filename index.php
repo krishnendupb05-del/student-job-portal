59f5b500cc48fa83ca6042b307f5463cef352e56
krishnendupb05-del <krishnendu.pb05@gmail.com>
2026-01-31 12:31:53 +0530

    Add initial index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Job Portal</title>
+</head>
+<body>
+    <h1>Welcome to Student Job Portal</h1>
+    <p>Project setup successful 🎉</p>
+</body>
+</html>
